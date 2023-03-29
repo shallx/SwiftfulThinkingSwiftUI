5442f1bb8b17b761c6550ce43e0f50a2fa1dfca2
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2023-03-29 17:13:26 +0600

    Working on bootcamp 5, time: 9:57

---
+++ SwiftfulThinkingBootcamp/GradientBootcamp.swift
@@ -0,0 +1,31 @@
+//
+//  GradientBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Macuser on 29/03/2023.
+//
+
+import SwiftUI
+
+struct GradientBootcamp: View {
+    var body: some View {
+        VStack(){
+            RoundedRectangle(cornerRadius: 26)
+                .fill(
+                    LinearGradient(gradient: Gradient(colors: [Color(uiColor: #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)), Color.pink]), startPoint: .leading, endPoint: .trailing)
+                )
+                .frame(width: 300, height: 200)
+            
+            RoundedRectangle(cornerRadius: 26)
+                .fill(RadialGradient(gradient: Gradient(colors: [Color.red, Color.yellow]), center: .leading, startRadius: 5, endRadius: 200))
+                .frame(width: 300, height: 200)
+            
+        }
+    }
+}
+
+struct GradientBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        GradientBootcamp()
+    }
+}
