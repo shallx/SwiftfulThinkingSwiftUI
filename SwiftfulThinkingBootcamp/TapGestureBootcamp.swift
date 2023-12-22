e983d2749d28965852ddf0d8726d7ef9d75e5152
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2023-12-22 18:40:52 +0600

    Added onTap

---
+++ SwiftfulThinkingBootcamp/TapGestureBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  TapGestureBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Macuser on 22/12/2023.
+//
+
+import SwiftUI
+
+struct TapGestureBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TapGestureBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TapGestureBootcamp()
+    }
+}
